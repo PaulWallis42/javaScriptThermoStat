a6f24ab7db9a1108f6e31ab6086b1318a14c6d45
Richard Lynch <rjlynch86@googlemail.com>
2015-04-14 10:31:35 +0100

    dont call methods the same as vars?

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,10 +1,16 @@
-describe('thermostat', function(){
+describe('Thermostat', function(){
 
-  var thermostat
+  var thermostat;
 
-  it('starts at 20 degrees', function(){
-    thermostat = new Thermostat
-    expect(thermostat.temperature()).toEqual(20)
-  }); 
+  it('starts at 20 degrees', function() {
+    thermostat = new Thermostat();
+    expect(thermostat.temperature()).toEqual(20);
+  });
+
+  it('increase the temperature by 1 degree with the up button', function() {
+    thermostat = new Thermostat();
+    thermostat.up();
+    expect(thermostat.temperature()).toEqual(21);
+  });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,11 @@
 var Thermostat = function(){
-  
+  this.temp = 20;
 };
 
-Thermostat.prototype.temperature = function() {
-  return 20;
+Thermostat.prototype.temperature = function(){
+  return this.temp;
+};
+
+Thermostat.prototype.up = function() {
+  this.temp ++;
 };
